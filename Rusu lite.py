7ca212aa2985611b7f00a0e34776b11480246399
Roy Paul Narido <79889856+roypaul21@users.noreply.github.com>
2021-08-15 20:19:09 +0800

    Update Rusu lite.py

--- Rusu lite.py
+++ Rusu lite.py
@@ -17,7 +17,7 @@ import itertools
 mydb = mysql.connector.connect(
        host = "127.0.0.1",
        user = "root",
-       password = "izakme123",
+       password = "*****",
        database =  "rusulite",
        )
 my_cursor = mydb.cursor()
